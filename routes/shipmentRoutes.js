97ee6f5300a010580b928ca50303a00f174bc531
codeblue12 <yasir.codeblue@gmail.com>
2023-12-29 10:48:40 +0500

    getShipment Api call from DHL

--- routes/shipmentRoutes.js
+++ routes/shipmentRoutes.js
@@ -3,8 +3,48 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 const Shipment = require('../models/shipmentModel');
-
+const {encode} = require('base-64')
+// const router = express.Router();
 const router = express.Router();
+// const fetch = require('node-fetch');
+
+const axios = require('axios');
+
+
+const getShipmentDocs = asyncHandler(async (req, res) => {
+    console.log(req.body);
+    // console.log('Request Headers:', req.headers);
+    requestBody = req.body;
+    const username = "apN2tG5lL6tE0e";
+    const password = "O!7iL@2vN$3nS$7m";
+    const credentials = `${username}:${password}`;
+    const base64Credentials = encode(credentials);
+    console.log(base64Credentials);
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Basic ${base64Credentials}`,
+        'Access-Control-Allow-Origin': '*',
+    };
+
+    const config = {
+        headers: headers,
+    };
+
+    const endpoint = 'https://express.api.dhl.com/mydhlapi/test/shipments';
+    console.log('_________________endpoint______________', endpoint);
+
+    try {
+        const response = await axios.post(endpoint, requestBody, config);
+        const result = response.data;
+        const documents = result.documents;
+        res.send(documents);
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 
 const uploadShipment = asyncHandler(async (req, res) => {
     try {
@@ -26,9 +66,9 @@ const uploadShipment = asyncHandler(async (req, res) => {
 
 const getAllShipments = asyncHandler(async (req, res) => {
     const shipmentsData = await Shipment.find();
-    const shipments = {shipmentsData}
+    const shipments = { shipmentsData }
     res.send(shipments);
-  });
+});
 
 router.post('/', uploadShipment);
 router.get('/getShipments', getAllShipments);
@@ -36,6 +76,8 @@ router.post('/get', (req, res) => {
     const jsonResponse = { message: 'this is debug' };
     res.send(jsonResponse);
 });
+
+router.post('/getDocuments', getShipmentDocs)
 
 
 module.exports = router;
