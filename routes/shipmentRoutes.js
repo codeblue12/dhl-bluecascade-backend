b830b841ade38354a2fb93248a0e6a580d1a1053
codeblue12 <yasir.codeblue@gmail.com>
2024-01-01 12:08:10 +0500

    error response send

--- routes/shipmentRoutes.js
+++ routes/shipmentRoutes.js
@@ -40,10 +40,10 @@ const getShipmentDocs = asyncHandler(async (req, res) => {
         const documents = result.documents;
         res.send(documents);
     } catch (error) {
-        console.error('Error:', error);
+        console.error('Error:', error.message);
         if (error.response) {
             console.error('Response Status:', error.response.status);
-            console.error('Response Data:', error.response.data.details);
+            console.error('Response Data:', error.response.data.detail);
         }
         res.status(500).send('Internal Server Error');
     }
